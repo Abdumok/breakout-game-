0e508eecad44810145998deda1d3370ca8900b95
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-02-23 19:40:47 +0300

    initial commit, setup main screen

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Screen
+
+#Setup Main Screen:
+window= Screen()
+window.title("Breakout Game v1.00")
+window.bgcolor("black")
+window.setup(width=1200, height=600)
+
+
+
+
+
+window.exitonclick()
